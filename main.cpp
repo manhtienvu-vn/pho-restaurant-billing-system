af4a28aee689923d2336af04723d91f5ea97dcf9
Tien Manh Vu <manhtienvu.vn@gmail.com>
2026-06-08 15:58:34 +0700

    "fix addOrder logic with required params: table_id, item_id, quantity"

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 
 enum TableStatus {
     AVAILABLE = 0,
-    OCCUPIED
+    OCCUPIED = 1
 };
 
 class MenuItem {
@@ -15,7 +15,9 @@ class MenuItem {
         std::string name;
         float price;
     public:
-        MenuItem (int ID, std::string n, float pr): id(ID), name(n), price(pr){};
+        MenuItem (int ID, std::string n, float pr): id(ID), name(n), price(pr){
+            std::cout << "[SUCCESS] Created new menu item: Name: " << name << " | ID: " << id << " | Price: " << price << "\n";
+        };
         float getPrice()
         {
             return price;
@@ -30,7 +32,7 @@ class MenuItem {
         }
         void showItem()
         {
-            std::cout << "Name: " << name << " | Price: " << price << "\n";
+            std::cout << "[INFO] ID: " << id << " | Name: " << name << " | Price: " << price << "\n";
         }
 
 };
@@ -40,19 +42,23 @@ class Menu {
         std::map<int, std::shared_ptr<MenuItem>> items;
     public:
         Menu(){};
-        void addItem(int id, std::string name, float price)
+
+        void addItemToMenu(int id, std::string name, float price)
         {
             std::shared_ptr<MenuItem> item = std::make_shared<MenuItem>(id, name, price);
             items.insert({id, item});
         }
-        std::shared_ptr<MenuItem> findItem(int id)
+
+        std::shared_ptr<MenuItem> findItemInMenu(int id)
         {
             auto it = items.at(id);
             return it;
         }
+
         void showMenu()
         {
             int count = 1;
+            std::cout << "--------RESTAURANT MENU--------" << "\n";
             for (const auto &pair : items){
                 std::cout << "1. Item ID: " << pair.first << " | ";
                 pair.second->showItem();
@@ -64,23 +70,28 @@ class OrderItem {
     private:
         std::shared_ptr<MenuItem> item;
         int quantity;
+
     public:
         OrderItem(std::shared_ptr<MenuItem> item, int quantity){
             this->item = item;
             this->quantity = quantity;
         }
+
         int getQuantity()
         {
             return this->quantity;
         }
+
         void setQuantity(int quantity)
         {
             this->quantity = quantity;
         }
+
         std::string getName()
         {
             return item->getName();
         }
+
         float calculateTotal()
         {
             return static_cast<float>(quantity * item->getPrice());
@@ -92,17 +103,21 @@ class Order {
         int order_id;
         std::vector <OrderItem> items;
         std::string created_time;
+
     public:
         Order(){};
-        void addItem(std::shared_ptr<MenuItem> item, int quantity)
+
+        void addItemToOrder(std::shared_ptr<MenuItem> item, int quantity)
         {
             OrderItem order_item(item, quantity);
             items.push_back(order_item);
         }
-        void removeItem(int id)
+
+        void removeItemFromOrder(int id)
         {
 
         }
+
         float calculateTotal()
         {
             float total = 0.0f;
@@ -111,13 +126,11 @@ class Order {
             }
             return total;
         }
-        void printBill()
-        {
-            float totalPrice = calculateTotal();
+
+        void showBill(){
             for (auto &v: items){
-                std::cout << "Item Name: " << v.getName() << " | Quantity: " << v.getQuantity() << "\n"; 
+                    std::cout << "Item Name: " << v.getName() << " | Quantity: " << v.getQuantity() << "\n"; 
             }
-            std::cout << "Total Price: " << totalPrice << "\n";
         }
 };
 
@@ -128,25 +141,51 @@ class Table {
         TableStatus status;
         std::unique_ptr <Order> current_order;
     public:
-        Table (int id): table_id(id), status(AVAILABLE) {};
+        Table (int id): table_id(id), status(AVAILABLE) {
+            /* Initially, no memory is allocated for the table since it's not opened */
+            current_order = nullptr;
+        }
+        
         void open()
         {
             status = OCCUPIED;
+            /* Only create order when the table is opened for service */
+            /* Allocate memory for an exclusive-ownership 'Order' object for this 'Table' */
             current_order = std::make_unique <Order>();
         }
+
         void close()
         {
             status = AVAILABLE;
-        }
-        bool isAvailable()
-        {
-            return status == AVAILABLE;
+            this->current_order.reset();
+            /* Delete the allocated memory for the exclusive-ownership 'Order' object */
         }
 
+        bool isOpened()
+        {
+            return status == OCCUPIED;
+        }
+
+        void addOrderItem(std::shared_ptr<MenuItem> item, int quantity)
+        {
+            if (status != OCCUPIED){
+                /* Return early if the table is currently closed */
+                return;
+            }
+            current_order->addItemToOrder(item, quantity);
+        }
+
+        void printBill()
+        {
+            float totalFees = current_order->calculateTotal();
+            current_order->showBill();
+            std::cout << "Total Fees: " << totalFees << "\n";
+        }
 };
 
 class Restaurant 
 {
+    /* Note: One restaurant should have multiple 'tables' with corresponding orders but only 01 'menu' */
     private:
         std::vector <Table> tables;    
         Menu menu;
@@ -154,29 +193,57 @@ class Restaurant
         int num_of_table;
     public:
         Restaurant(int n){
+
+            /* Initially, create a vector of 'num_of_table' tables for the restaurant*/
             num_of_table = n;
             for (int i = 1; i <= n; i++)
             {
+                /* Create a new table with id 'i' */
                 Table new_table (i);
+                /* Use std::move() because there exists unique pointer in each Table object */
                 tables.push_back(std::move(new_table));
+                /* Then we wish to transfer ownership of these objects to the vector elements */
             }
             std::cout << "[SUCCESS] Created a restaurant with " << num_of_table << " tables" << "\n";
+
+            /* Create a list of menu item */
+            menu.addItemToMenu(1, "Pho Bo", 30000);
+            menu.addItemToMenu(2, "Pho Ga", 30000);
+            menu.addItemToMenu(3, "Pho Bo Ga", 35000);
         }
 
         void openTable(int table_id)
         {
             tables[table_id-1].open();
-            std::cout << "[SUCCESS] Add new table with ID: "  << table_id <<  "\n";
+            std::cout << "[SUCCESS] Table with ID: "  << table_id << " is now opened for service!" << "\n";
         }
 
-        void addOrder(int table_id)
+        void addOrder(int table_id, int item_id, int quantity)
         {
-            
+            if (table_id < 0 || table_id > this->num_of_table){
+                std::cout << "[ERROR]: Invalid Table ID. Try again!" << "\n";
+                return;
+            } else if (!tables[table_id-1].isOpened()){
+                std::cout << "[ERROR] Table with ID: " << table_id << " is currently closed!" << "\n";
+                return;
+            } else if (quantity <= 0)
+            {
+                std::cout << "[ERROR] Invalid quantity. Try again!" << "\n";
+                return;
+            }
+            std::shared_ptr<MenuItem> item = menu.findItemInMenu(item_id);
+            tables[table_id-1].addOrderItem(item, quantity);
+            std::cout << "[SUCCESS] Ordered " << quantity << " " << item->getName() << " for Table with ID: " << table_id << "\n";
         }
 
         void checkOut(int table_id)
         {
-
+            if(!tables[table_id-1].isOpened())
+            {
+                return;
+            }
+            tables[table_id-1].printBill();
+            tables[table_id-1].close();
         }
 
         void showRevenue()
@@ -189,5 +256,6 @@ int main(void)
 {
     Restaurant restaurant(10);
     restaurant.openTable(1);
+    restaurant.addOrder(1, 1, 2);
     return 0;
 }
